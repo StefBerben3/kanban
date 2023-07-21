3734c406c55fbf2bd07c119e97759fc53ed54d70
stef <stefberbuh@hotmail.com>
2023-07-21 11:35:27 +0200

    websockets toegevoegd

--- kanban/client/src/kanbanboard.js
+++ kanban/client/src/kanbanboard.js
@@ -1,27 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./style/modal.css";
-import axios from 'axios';
+import axios from "axios";
 
 const KanbanBoard = () => {
   const [tasks, setCards] = useState([]);
+  const [ws, setWebSocket] = useState(null);
+  const [editingCard, setEditCard] = useState(null);
+  const [editedTitle, setEditTitle] = useState("");
+  const [editedDescription, setEditDescription] = useState("");
+  const [editedPriority, setEditPriority] = useState(1);
+  const [selectedCard, setSelectedCard] = useState(null);
+  
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:3001");
+
+    socket.onopen = () => {
+      console.log("WebSocket connection opened");
+      setWebSocket(socket);
+    };
+
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      handleWebSocketMessage(message);
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket connection closed");
+      setWebSocket(null);
+    };
+
+    return () => {
+      socket.close();
+    };
+  }, []);
 
   useEffect(() => {
     fetchCards();
-  });
-  
+  }, []);
 
   const fetchCards = async () => {
-
-      const response = await axios.get('http://localhost:3001/cards');
-      setCards(response.data);
-   
+    const response = await axios.get("http://localhost:3001/cards");
+    setCards(response.data);
   };
-
-  const [editingCard, setEditCard] = useState(null);
-  const [editedTitle, setEditTitle] = useState('');
-  const [editedDescription, setEditDescription] = useState('');
-  const [editedPriority, setEditPriority] = useState(1);
-  const [selectedCard, setSelectedCard] = useState(null);
 
   const openModal = (card) => {
     setSelectedCard(card);
@@ -45,20 +65,19 @@ const KanbanBoard = () => {
       description: editedDescription,
       priority: editedPriority,
     };
-  
+
     const updatedTasks = tasks.map((task) =>
       task.id === editingCard.id ? updatedCard : task
     );
-  
+
     setCards(updatedTasks);
     setEditCard(null);
-  
     updateCardInDatabase(updatedCard);
   };
 
   const dropCards = (e, targetColumn) => {
     e.preventDefault();
-    const taskId = e.dataTransfer.getData('text/plain');
+    const taskId = e.dataTransfer.getData("text/plain");
     const updatedTasks = tasks.map((task) =>
       task.id.toString() === taskId ? { ...task, column: targetColumn } : task
     );
@@ -66,15 +85,15 @@ const KanbanBoard = () => {
   };
 
   const startDrag = (e, taskId) => {
-    e.dataTransfer.setData('text/plain', taskId);
+    e.dataTransfer.setData("text/plain", taskId.toString());
   };
 
   const createCard = async (column) => {
-    const priority = prompt('Enter Priority (1-5):');
+    const priority = prompt("Enter Priority (1-5):");
     if (priority === null) return;
-    const title = prompt('Enter Title:');
+    const title = prompt("Enter Title:");
     if (title === null) return;
-    const description = prompt('Enter Description:');
+    const description = prompt("Enter Description:");
     if (description === null) return;
 
     const newCard = {
@@ -84,72 +103,96 @@ const KanbanBoard = () => {
       column_name: column,
     };
 
-  
-      const response = await axios.post('http://localhost:3001/cards', newCard);
-      setCards([...tasks, response.data]);
-   
+    sendWebSocketMessage({ type: "NEW_CARD", card: newCard });
+
+    const response = await axios.post("http://localhost:3001/cards", newCard);
+    setCards([...tasks, response.data]);
   };
 
-  const deleteCard = async (taskId) => {
-    const updatedTasks = tasks.map((task) =>
-      task.id === taskId ? { ...task, deleted: 1 } : task
-    );
-    setCards(updatedTasks);
-  
-   
-      await axios.patch(`http://localhost:3001/cards/${taskId}`, { deleted: 1 });
+  const sendWebSocketMessage = (message) => {
 
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify(message));
+    } else {
+      console.log("WebSocket connection is not open.");
+    }
+  };
+
+  const handleWebSocketMessage = (message) => {
+    if (message.type === "NEW_CARD") {
+
+      setCards((prevCards) => [...prevCards, message.card]);
+    } else if (message.type === "DELETE_CARD") {
+
+      setCards((prevCards) =>
+        prevCards.filter((card) => card.id !== message.cardId)
+      );
+    }
+  };
+
+  const deleteCard = async (cardId) => {
+    try {
+      await axios.patch(`http://localhost:3001/cards/${cardId}`, {
+        deleted: 1,
+      });
+
+
+      sendWebSocketMessage({ type: "DELETE_CARD", cardId });
+      setCards((prevCards) => prevCards.filter((card) => card.id !== cardId));
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const updateCardInDatabase = async (updatedCard) => {
-      await axios.put(`http://localhost:3001/cards/${updatedCard.id}`, updatedCard);
+    await axios.put(
+      `http://localhost:3001/cards/${updatedCard.id}`,
+      updatedCard
+    );
   };
 
   return (
     <div className="container mx-auto py-8">
-    <div className="grid grid-cols-3 gap-4">
-      {['To Do', 'In Progress', 'Done'].map((column) => (
-        <div key={column}>
-          <h2 className="text-xl font-bold mb-4">{column}</h2>
-          <div
-            className="bg-gray-100 p-4 rounded shadow space-y-4"
-            onDrop={(e) => dropCards(e, column)}
-            onDragOver={(e) => e.preventDefault()}>
-            {tasks
-              .filter((task) => task.column_name === column)
-              .map((task) => (
-                <div
-                  key={task.id}
-                  className="bg-white p-4 rounded shadow cursor-pointer flex items-center justify-between"
-                  draggable
-                  onDragStart={(e) => startDrag(e, task.id.toString())}
-                  onClick={() => openModal(task)}>
-                  <div>
-                    {task.title} - Priority: {task.priority}
+      <div className="grid grid-cols-3 gap-4">
+        {["To Do", "In Progress", "Done"].map((column) => (
+          <div key={column}>
+            <h2 className="text-xl font-bold mb-4">{column}</h2>
+            <div
+              className="bg-gray-100 p-4 rounded shadow space-y-4"
+              onDrop={(e) => dropCards(e, column)}
+              onDragOver={(e) => e.preventDefault()}>
+              {tasks
+                .filter((task) => task.column_name === column)
+                .map((task) => (
+                  <div key={task.id}>
+                    <div
+                      className="bg-white p-4 rounded shadow cursor-pointer flex items-center justify-between"
+                      draggable
+                      onDragStart={(e) => startDrag(e, task.id)}
+                      onClick={() => openModal(task)}>
+                      <div>
+                        {task.title} - Priority: {task.priority}
+                      </div>
+                    </div>
                   </div>
-                  <button onClick={() => deleteCard(task.id)} className="text-red-500">
-                  </button>
-              </div>
-            ))}
-        </div>
-        <button
-          onClick={() => createCard(column)}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600">Create New Card
-        </button>
+                ))}
+            </div>
+            <button
+              onClick={() => createCard(column)}
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600">Create New Card
+            </button>
           </div>
-          
         ))}
-        </div>
-      
-      {
-      selectedCard && (
-        <div className='modal-overlay'>
-          <div className='modal'>
+      </div>
+
+      {selectedCard && (
+        <div className="modal-overlay">
+          <div className="modal">
             {editingCard ? (
               <div>
                 <h2>Edit Card</h2>
-                
-                <label htmlFor="title">Title:</label>  <input
+                <label htmlFor="title">Title:</label>{" "}
+                <input
                   type="text"
                   value={editedTitle}
                   onChange={(e) => setEditTitle(e.target.value)}/>
@@ -165,13 +208,15 @@ const KanbanBoard = () => {
                 <button onClick={updateCard}>Update</button>
                 <button onClick={() => setEditCard(null)}>Cancel</button>
               </div>
-              ) : (
+            ) : (
               <div>
                 <h2>Task: {selectedCard.title}</h2>
                 <p>Priority: {selectedCard.priority}</p>
                 <p>Description: {selectedCard.description}</p>
                 <button onClick={() => editCard(selectedCard)}>Edit</button>
-                <button onClick={() => deleteCard(selectedCard.id)}>Delete</button>
+                <button onClick={() => deleteCard(selectedCard.id)}>
+                  Delete
+                </button>
                 <button onClick={closeModal}>Close</button>
               </div>
             )}
--- kanban/server/index.js
+++ kanban/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql')
 const cors = require('cors');
+const WebSocket = require("ws");
 
 const db =  mysql.createPool({
     host: "localhost",
@@ -13,6 +14,10 @@ const db =  mysql.createPool({
 app.use(express.json());
 app.use(cors());
 
+// app.listen(3001,()=>{
+//     console.log("server  is listening  on port 3001");
+// });
+
 app.get("/cards",(req,res)=>{
     const query =  "select  *  from  kanban_cards Where deleted = 0";
     db.query(query,(err,data)=>{
@@ -20,25 +25,6 @@ app.get("/cards",(req,res)=>{
     return res.json(data)
     })
 })
-
-app.post('/cards', (req, res) => {
-    const { title, description, priority, column_name } = req.body;
-  
-    const newCard = {
-      title,
-      description,
-      priority,
-      column_name,
-    };
-  
-    db.query('INSERT INTO kanban_cards SET ?', newCard, (err, result) => {
-      if (err) {
-        console.error(err);
-      } else {
-        newCard.id = result.insertId;
-      }
-    });
-  });
 
 
   app.put('/cards/:id', (req, res) => {
@@ -56,15 +42,60 @@ app.post('/cards', (req, res) => {
 
   app.patch('/cards/:id', (req, res) => {
     const { id } = req.params;
-  
     db.query('UPDATE kanban_cards SET deleted = 1 WHERE id = ?', id, (err, result) => {
       if (err) {
         console.error(err);
-
-      } 
+      } else {
+        const message = { type: 'DELETE_CARD', cardId: id };
+        broadcastMessage(message);
+        return res.status(200).json({ success: true });
+      }
     });
   });
 
-app.listen(3001,()=>{
-    console.log("server  is listening  on port 3001");
+const server = app.listen(3001, () => {
+  console.log("WebSocket-server gestart op poort 3001");
+});
+const wss = new WebSocket.Server({ server });
+
+const clients = new Set();
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  console.log("Nieuwe client verbonden");
+
+  ws.on("close", () => {
+    clients.delete(ws);
+    console.log("Client verbinding gesloten");
+  });
+});
+
+const broadcastMessage = (message) => {
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+};
+
+app.post("/cards", (req, res) => {
+  const { title, description, priority, column_name } = req.body;
+
+  const newCard = {
+    title,
+    description,
+    priority,
+    column_name,
+  };
+
+  db.query('INSERT INTO kanban_cards SET ?', newCard, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      newCard.id = result.insertId;
+    }
+  });
+
+  broadcastMessage({ type: "NEW_CARD", card: newCard });
+
+  res.status(201).json(newCard);
 });
